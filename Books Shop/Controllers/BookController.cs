bec540d112f2febfdabf18a9603a69811e8f81c2
MohamedFarghly <mh.aljabry7@gmail.com>
2021-12-08 15:11:05 +0200

    Add BookControllers

---
+++ Books Shop/Controllers/BookController.cs
@@ -0,0 +1,164 @@
+﻿using Books_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Books_Shop.Controllers
+{
+    public class BookController : Controller
+    {
+        AppDbContext _appDbContext = new AppDbContext();
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            try
+            {
+                if (Session["UserName"] != null)
+                {
+                    List<Book> books = _appDbContext.Books.ToList();
+                    return View(books);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message.ToString();
+            }
+            return RedirectToAction("Login", "User");
+
+        }
+
+        // Borrow A Book
+        public ActionResult Borrow(int Id)
+        {
+            try
+            {
+                Book book = _appDbContext.Books.FirstOrDefault(b => b.Id == Id);
+                if (book != null)
+                {
+                    if (book.CurrentEditions == 0)
+                    {
+                        ViewBag.noLeft = "No Left Editions";
+                        return Json(new { success = false, message = "No Left Editions" }, JsonRequestBehavior.AllowGet); ;
+                    }
+
+                    string userName = Session["UserName"].ToString();
+                    User currentUser = _appDbContext.Users.FirstOrDefault(u => u.UserName == userName);
+                    UserBooks user = _appDbContext.UserBooks.FirstOrDefault(u => u.users.UserName == userName);
+
+                    if (user != null)
+                    {
+                        var existBook = user.users.books.FirstOrDefault(b => b.Id == Id);
+
+                        if (existBook != null)
+                        {
+                            UserBooks currentBook = _appDbContext.UserBooks.FirstOrDefault(b => b.books.Id == Id && b.users.UserName == userName);
+                            currentBook.NumberOfEditions += 1;
+
+                            //Update the users table
+                            _appDbContext.Entry(currentBook).State = EntityState.Modified;
+                        }
+
+                        //New Book
+                        else
+                        {
+                            _appDbContext.UserBooks.Add(new UserBooks()
+                            {
+                                users = currentUser,
+                                books = book,
+                                NumberOfEditions = 1
+                            });
+
+                            currentUser.books.Add(book);
+                        }
+                    }
+
+                    //New User
+                    else
+                    {
+                        _appDbContext.UserBooks.Add(new UserBooks()
+                        {
+                            users = currentUser,
+                            books = book,
+                            NumberOfEditions = 1
+                        });
+
+                        currentUser.books.Add(book);
+                    }
+
+                    book.CurrentEditions -= 1;
+
+                    _appDbContext.Entry(currentUser).State = EntityState.Modified;
+                    _appDbContext.SaveChanges();
+                }
+                return Json(new { success = true, JsonRequestBehavior.AllowGet });
+            }
+
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // Return A Book
+        public ActionResult Return(int Id)
+        {
+            try
+            {
+                Book book = _appDbContext.Books.FirstOrDefault(b => b.Id == Id);
+                if (book != null)
+                {
+                    // All book Editions returned
+                    if (book.CurrentEditions == book.TotalEditions)
+                    {
+                        ViewBag.noLeft = "No More Edition For This Book, All Edition Returned";
+                        return Json(new { success = false, message = "No More Edition For This Book, All Edition Returned" }, JsonRequestBehavior.AllowGet); ;
+                    }
+
+                    string userName = Session["UserName"].ToString();
+                    UserBooks user = _appDbContext.UserBooks.FirstOrDefault(u=>u.users.UserName == userName);
+
+                    if (user != null)
+                    {
+                        var existBook = user.users.books.FirstOrDefault(b => b.Code == book.Code);
+                        UserBooks currentBook = _appDbContext.UserBooks.FirstOrDefault(b => b.books.Id == Id && b.users.UserName == userName);
+                        if (existBook != null && currentBook.NumberOfEditions > 0)
+                        {
+                           
+                            currentBook.NumberOfEditions -= 1;
+
+                            //Update the users table
+                            _appDbContext.Entry(currentBook).State = EntityState.Modified;
+                        }
+                        else if (existBook !=null)
+                        {
+                            if (currentBook.NumberOfEditions == 0)
+                            {
+                                user.users.books.Remove(existBook);
+                                _appDbContext.Entry(user.users).State = EntityState.Modified;
+                                _appDbContext.SaveChanges();
+
+                            }
+                            return Json(new { success = false, message = "You returned all borrowed books " }, JsonRequestBehavior.AllowGet);
+                        }
+                    }
+
+                    book.CurrentEditions += 1;
+
+                    _appDbContext.Entry(book).State = EntityState.Modified;
+                    _appDbContext.SaveChanges();
+                }
+                return Json(new { success = true, JsonRequestBehavior.AllowGet });
+            }
+
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
