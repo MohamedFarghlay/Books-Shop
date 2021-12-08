397997a34d9e570fb31fc05038500d95897f2075
MohamedFarghly <mh.aljabry7@gmail.com>
2021-12-08 15:13:06 +0200

    Add Helpers (Hashing, Code Generator)

---
+++ Books Shop/Helpers/CodeGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Books_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books_Shop.Helpers
+{
+    public class CodeGenerator
+    {
+        private static Random _random = new Random();
+        private static AppDbContext _appDbContext = new AppDbContext();
+        public static string generate(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string Code = new string(Enumerable.Repeat(chars, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+            List<User> users = _appDbContext.Users.ToList();
+            if (users != null)
+            {
+                int i = 0;
+                foreach (var user in users)
+                {
+                    if ( user.Code == Code)
+                    {
+                        Code = new string(Enumerable.Repeat(chars, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+                    }
+                    else{
+                        return Code;
+                    }
+                    i++;
+                }
+            }
+            return Code;
+
+        }
+    }
+}
---
+++ Books Shop/Helpers/Hashing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Books_Shop.Helpers
+{
+    public class Hashing
+    {
+        public static string getHash(string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            SHA256Managed hashCode = new SHA256Managed();
+            byte[] hash = hashCode.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte b in hash)
+            {
+                hashString += String.Format("{0:x2}", b);
+            }
+            return hashString;
+        }
+    }
+}
